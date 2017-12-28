785e45ed9d57556923459f12b32ff69de8f34856
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-28 17:59:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 	programmer_hash =
  		{
@@ -83,6 +82,7 @@ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LI
         }
      }
 
+		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 end
 
